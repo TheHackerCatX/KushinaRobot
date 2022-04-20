fc03e506f2415c12c7adf94f0f61750b7b8582a8
TheHackerCatX <99822949+TheHackerCatX@users.noreply.github.com>
2022-04-21 01:04:07 +0530

    update reporting_sql.py

--- Yone/Database/reporting_sql.py
+++ Yone/Database/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from Yone.Database import BASE, SESSION
+from Kushina.Database import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, String
 
 
