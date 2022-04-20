3f3418704a28ccc1009a56fa8448dcc59143a46d
TheHackerCatX <99822949+TheHackerCatX@users.noreply.github.com>
2022-04-21 00:45:29 +0530

    update validations.py

--- Yone/Handlers/telethon/validations.py
+++ Yone/Handlers/telethon/validations.py
@@ -1,5 +1,5 @@
-from Yone.Handlers.telethon import IMMUNE_USERS, telethn
-from Yone import REQUESTER
+from kushina.Handlers.telethon import IMMUNE_USERS, telethn
+from kushina import REQUESTER
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
@@ -42,13 +42,13 @@ async def is_user_admin(user_id: int, chat_id):
     return status
 
 
-async def yone_is_admin(chat_id: int):
+async def kushina_is_admin(chat_id: int):
     status = False
-    yone = await telethn.get_me()
+    kushina = await telethn.get_me()
     async for user in telethn.iter_participants(
         chat_id, filter=ChannelParticipantsAdmins
     ):
-        if yone.id == user.id:
+        if kushina.id == user.id:
             status = True
             break
     return status
