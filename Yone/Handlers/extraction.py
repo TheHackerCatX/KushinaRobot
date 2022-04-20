f4422c1241ea6f36e3d3635d64fb247ebbe98dc4
TheHackerCatX <99822949+TheHackerCatX@users.noreply.github.com>
2022-04-21 00:52:53 +0530

    update extraction.py

--- Yone/Handlers/extraction.py
+++ Yone/Handlers/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from Yone import LOGGER
-from Yone.Plugins.users import get_user_id
+from Kushina import LOGGER
+from Kushina.Plugins.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
