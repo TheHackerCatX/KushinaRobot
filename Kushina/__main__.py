26dd4088f5bdb682409de575a7327444d0b023bd
TheHackerCatX <99822949+TheHackerCatX@users.noreply.github.com>
2022-04-22 09:55:08 +0530

    Update __main__.py

--- Kushina/__main__.py
+++ Kushina/__main__.py
@@ -233,6 +233,13 @@ def kushina_about_callback(update: Update, context: CallbackContext):
             \n ‣ I have Note Keeping System, Blacklists, And even Predetermined replies on certain keywords.
             \n ‣ I check Admins Permissions before perform any Command and more Stuffs.
             \n ‣ I have an advanced Artificial Chatbot System, so can talk with users like humans.
+            \n\n ★ 𝙼𝚈 𝙽𝙰𝙼𝙴: KushinaRobot
+            \n ★ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: Ryoishin
+            \n ★ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: Telethon
+            \n ★ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: Python 3
+            \n ★ 𝙳𝙰𝚃𝙰𝙱𝙰𝚂𝙴: Mongodb
+            \n ★ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: Linux
+            \n ★ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v0.1.2 [ Beta ]
             \n\n*If you have any Question, You can join Support Chat. My Developer Team will Answer. Check Link Below*""".format(
                         escape_markdown(first_name), escape_markdown(context.bot.first_name)),
             parse_mode=ParseMode.MARKDOWN,
